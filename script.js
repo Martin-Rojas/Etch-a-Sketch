5e2efe8f6b976cecf6de4bfd625985d063cd28eb
Martin-Rojas <rojas.martin25@yahoo.com>
2023-11-20 17:41:31 -0800

    I update the functionality of reset btn and input value to get the size of grid

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
 const grid = document.querySelector(".grid");
 const div = document.createElement("div");
 const userColorChoiceElement = document.querySelector("#color");
+const gridSizeInput = document.getElementById("grid-size");
+const resetBntElement = document.getElementById("reset");
 
-let userColor = 'black';
+let userColor = "black";
+const defaultValue = 16;
 
+/* reset background color to it original color*/
+resetBntElement.addEventListener("click", () => {
+  let allDivs = document.querySelectorAll(".single-div");
+  allDivs.forEach((div) => {
+    div.style.backgroundColor = `white`;
+  });
+});
+
+/* User change a color for background-color*/
 userColorChoiceElement.addEventListener("change", (event) => {
   userColor = event.target.value;
   return userColor;
 });
 
-const defaultValue = 16;
+/* User chose the size of a grid*/
+gridSizeInput.addEventListener("change", (e) => {
+  //delete grid
+  let allDivs = document.querySelectorAll(".single-div");
+  allDivs.forEach((div) => {
+    div.remove();
+  });
+
+  let userSizeGrid = e.target.value;
+  /*Create a grid with dimensions user gave */
+  for (let rows = 0; rows < userSizeGrid; rows++) {
+    for (let colms = 0; colms < userSizeGrid; colms++) {
+      const div = document.createElement("div");
+      div.style.width = `${grid.clientWidth / userSizeGrid}px`;
+      div.style.height = `${grid.clientWidth / userSizeGrid}px`;
+      div.classList.add("single-div");
+      div.classList.add("cells");
+      div.addEventListener("mouseover", (e) => {
+        div.style.backgroundColor = userColor;
+      });
+
+      grid.append(div);
+    }
+  }
+});
 
 const createGrid = (sizeGrid) => {
   /*Create a grid with dimensions user gave */
@@ -19,7 +55,7 @@ const createGrid = (sizeGrid) => {
       div.style.width = `${grid.clientWidth / sizeGrid}px`;
       div.style.height = `${grid.clientWidth / sizeGrid}px`;
       div.classList.add("single-div");
-
+      div.classList.add("cells");
       div.addEventListener("mouseover", (e) => {
         div.style.backgroundColor = userColor;
       });
