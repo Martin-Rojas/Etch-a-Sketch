3fa58e16b1acd52b1bfc1035bbc557311a830824
Martin-Rojas <rojas.martin25@yahoo.com>
2023-11-21 18:11:00 -0800

    update feature random color and done with project

--- script.js
+++ script.js
@@ -4,9 +4,26 @@ const userColorChoiceElement = document.querySelector("#color");
 const gridSizeInput = document.getElementById("grid-size");
 const resetBntElement = document.getElementById("reset");
 const btnBlackElement = document.getElementById("black");
+const btnRGBElement = document.getElementById("rgb");
 
 let userColor = "black";
 const defaultValue = 16;
+
+// Get random color
+const getRandomColor = () => {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  return `#${randomColor}`;
+};
+
+btnRGBElement.addEventListener("click", () => {
+  let val = document.getElementById("grid-size").value;
+  let cell = grid.children;
+  for (let i = 0; i < val * val; i++) {
+    cell[i].addEventListener("mouseover", function (event) {
+      event.target.style.backgroundColor = getRandomColor();
+    });
+  }
+});
 
 /* reset background color to it original color*/
 resetBntElement.addEventListener("click", () => {
@@ -18,14 +35,25 @@ resetBntElement.addEventListener("click", () => {
 
 /* btn will change userColor to black*/
 btnBlackElement.addEventListener("click", () => {
-  userColor = `black`;
-  return userColor;
+  let val = document.getElementById("grid-size").value;
+  let cell = grid.children;
+  for (let i = 0; i < val * val; i++) {
+    cell[i].addEventListener("mouseover", function (event) {
+      event.target.style.backgroundColor = `black`;
+    });
+  }
 });
 
 /* User change a color for background-color*/
 userColorChoiceElement.addEventListener("change", (event) => {
   userColor = event.target.value;
-  return userColor;
+  let val = document.getElementById("grid-size").value;
+  let cell = grid.children;
+  for (let i = 0; i < val * val; i++) {
+    cell[i].addEventListener("mouseover", function (event) {
+      event.target.style.backgroundColor = userColor;
+    });
+  }
 });
 
 /* User chose the size of a grid*/
@@ -37,6 +65,7 @@ gridSizeInput.addEventListener("change", (e) => {
   });
 
   let userSizeGrid = e.target.value;
+
   /*Create a grid with dimensions user gave */
   for (let rows = 0; rows < userSizeGrid; rows++) {
     for (let colms = 0; colms < userSizeGrid; colms++) {
@@ -44,7 +73,6 @@ gridSizeInput.addEventListener("change", (e) => {
       div.style.width = `${grid.clientWidth / userSizeGrid}px`;
       div.style.height = `${grid.clientWidth / userSizeGrid}px`;
       div.classList.add("single-div");
-
       div.addEventListener("mouseover", (e) => {
         div.style.backgroundColor = userColor;
       });
@@ -63,7 +91,7 @@ const createGrid = (sizeGrid) => {
       div.style.height = `${grid.clientWidth / sizeGrid}px`;
       div.classList.add("single-div");
 
-      div.addEventListener("mouseover", (e) => {
+      div.addEventListener("mouseover", () => {
         div.style.backgroundColor = userColor;
       });
 
